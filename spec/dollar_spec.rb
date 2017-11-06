4acca3e1c4c9b5a6019047d55b91b85525f8670a
YuSakaguchi <y_sakaguchi@libertyfish.co.jp>
2017-11-06 16:51:52 +0900

    第3章 三角測量

--- spec/dollar_spec.rb
+++ spec/dollar_spec.rb
@@ -1,9 +1,8 @@
 require 'spec_helper'
 
 RSpec.describe Dollar do
-  let(:dollar) { Dollar.new(5) }
-
   describe "Multipulication" do
+    let(:dollar) { Dollar.new(5) }
     it "2 times" do
       product = dollar.amount_times(2)
       expect(product.amount).to eq 10
@@ -14,4 +13,19 @@ RSpec.describe Dollar do
       expect(product.amount).to eq 15
     end
   end
+
+  describe "Equality" do
+    it "object match true" do
+      dollar1 = Dollar.new(1)
+      dollar2 = dollar1
+      expect(dollar1.equal?(dollar2)).to be true
+    end
+
+    it "object match false" do
+      dollar1 = Dollar.new(1)
+      dollar2 = Dollar.new(2)
+      expect(dollar1.equal?(dollar2)).to be false
+    end
+  end
+
 end
