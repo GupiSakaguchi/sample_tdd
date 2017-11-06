0eecabf712584702d6226bfb58954b42ece04f36
YuSakaguchi <y_sakaguchi@libertyfish.co.jp>
2017-11-06 16:40:36 +0900

    第2章 明白な実装

--- lib/dollar.rb
+++ lib/dollar.rb
@@ -1,9 +1,11 @@
 class Dollar
+  attr_reader :amount
+
   def initialize(amount)
     @amount = amount
   end
 
   def amount_times(times)
-    @amount * times
+    Dollar.new(@amount * times)
   end
 end
--- spec/dollar_spec.rb
+++ spec/dollar_spec.rb
@@ -1,9 +1,17 @@
 require 'spec_helper'
 
 RSpec.describe Dollar do
-  it "Multipulication" do
-    dollar = Dollar.new(5)
-    result = dollar.amount_times(2)
-    expect(result).to eq 10
+  let(:dollar) { Dollar.new(5) }
+
+  describe "Multipulication" do
+    it "2 times" do
+      product = dollar.amount_times(2)
+      expect(product.amount).to eq 10
+    end
+
+    it "3 times" do
+      product = dollar.amount_times(3)
+      expect(product.amount).to eq 15
+    end
   end
 end
