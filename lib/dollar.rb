9d9d897078d993fde259d7c6c4e759d1936e627c
GupiSakaguchi <gupisakaguchi@gmail.com>
2017-11-07 02:49:16 +0900

    第7章 疑念をテストに翻訳する 第8章 実装を隠す

--- lib/dollar.rb
+++ lib/dollar.rb
@@ -10,11 +10,24 @@ class Money
   end
 
   def equals(object)
-    @amount == object.amount
+    @amount == object.amount && self.class.name == object.class.name
+  end
+
+  class << self
+    def dollar(amount)
+      Dollar.new(amount)
+    end
+
+    def franc(amount)
+      Franc.new(amount)
+    end
   end
 end
 
 class Dollar < Money
+  def amount_times(times)
+    Dollar.new(@amount * times)
+  end
 end
 
 class Franc < Money
--- spec/dollar_spec.rb
+++ spec/dollar_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 RSpec.describe Dollar do
   describe "Multipulication" do
-    let(:dollar) { Dollar.new(5) }
+    let(:dollar) { Money.dollar(5) }
     it "2 times" do
       expect(Dollar.new(10).amount).to eq dollar.amount_times(2).amount
     end
@@ -30,27 +30,27 @@ end
 
 RSpec.describe Franc do
   describe "Multipulication" do
-    let(:dollar) { Franc.new(5) }
+    let(:franc) { Money.franc(5) }
     it "2 times" do
-      expect(Franc.new(10).amount).to eq dollar.amount_times(2).amount
+      expect(Franc.new(10).amount).to eq franc.amount_times(2).amount
     end
 
     it "3 times" do
-      expect(Franc.new(15).amount).to eq dollar.amount_times(3).amount
+      expect(Franc.new(15).amount).to eq franc.amount_times(3).amount
     end
   end
 
   describe "Equality" do
     it "object match true" do
-      dollar1 = Franc.new(1)
-      dollar2 = Franc.new(1)
-      expect(dollar1.equals(dollar2)).to be true
+      franc1 = Franc.new(1)
+      franc2 = Franc.new(1)
+      expect(franc1.equals(franc2)).to be true
     end
 
     it "object match false" do
-      dollar1 = Franc.new(1)
-      dollar2 = Franc.new(2)
-      expect(dollar1.equals(dollar2)).to be false
+      franc1 = Franc.new(1)
+      franc2 = Franc.new(2)
+      expect(franc1.equals(franc2)).to be false
     end
   end
 
